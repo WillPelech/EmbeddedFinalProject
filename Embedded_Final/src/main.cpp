d955c28a8c8136d1364eccacf06c13f4798262d9
WillScooby <wpelech23@gmail.com>
2025-04-17 14:13:19 -0400

    working FFT test code

--- Embedded_Final/src/main.cpp
+++ Embedded_Final/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <arduinoFFT.h>
 
 enum Level {
     LEVEL_0,
@@ -10,13 +11,38 @@ enum Level {
     LEVEL_6,
     LEVEL_7,
     LEVEL_8,
-    LEVEL_9
+    LEVEL_9,
+    LEVEL_10
 };
 
+void FFT_test() {
+    int samples = 10; 
+    int samplingFrequency = 1000; 
+    double frequency = 50; 
+
+    double vReal[samples];
+    double vImag[samples];
+
+    for (int i = 0; i < samples; i++) {
+        vReal[i] = 100 * sin(2 * PI * frequency * i / samplingFrequency); 
+        vImag[i] = 0; 
+    }
+
+    Serial.println("Sine Wave Values:");
+    for (int i = 0; i < samples; i++) {
+        Serial.print("Sample ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(vReal[i]);
+    }
+}
+
 void setup() {
-
+    Serial.begin(9600);
+    delay(2000);
+    FFT_test();
 }
+
 void loop() {
-
+    
 }
-
--- Embedded_Final/src/will.cpp
+++ Embedded_Final/src/will.cpp
@@ -1,43 +1,53 @@
-#include <Arduino.h>
-#include <arduinoFFT.h>
+// #include <Arduino.h>
+// #include <arduinoFFT.h>
 
 
 
 
-enum Level {
-    LEVEL_0,
-    LEVEL_1,
-    LEVEL_2,
-    LEVEL_3,
-    LEVEL_4,
-    LEVEL_5,
-    LEVEL_6,
-    LEVEL_7,
-    LEVEL_8,
-    LEVEL_9,
-    LEVEL_10
-};
+// enum Level {
+//     LEVEL_0,
+//     LEVEL_1,
+//     LEVEL_2,
+//     LEVEL_3,
+//     LEVEL_4,
+//     LEVEL_5,
+//     LEVEL_6,
+//     LEVEL_7,
+//     LEVEL_8,
+//     LEVEL_9,
+//     LEVEL_10
+// };
 
-void FFT_test(){
-int samples = 10;//this will be the number of samples
-int sampling_freq = 1000;
+// void FFT_test() {
+//     int samples = 10; // Number of samples
+//     int samplingFrequency = 1000; // Sampling frequency in Hz
+//     double frequency = 50; // Frequency of the sine wave in Hz
 
+//     double vReal[samples];
+//     double vImag[samples];
 
-double vReal[samples];
-double vImag[samples];
-ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, samples, samplingFrequency);
+//     // Fill vReal with a sine wave
+//     for (int i = 0; i < samples; i++) {
+//         vReal[i] = 100 * sin(2 * PI * frequency * i / samplingFrequency); // Amplitude of 100
+//         vImag[i] = 0; // Imaginary part is 0
+//     }
 
+//     // Print the sine wave values
+//     Serial.println("Sine Wave Values:");
+//     for (int i = 0; i < samples; i++) {
+//         Serial.print("Sample ");
+//         Serial.print(i);
+//         Serial.print(": ");
+//         Serial.println(vReal[i]);
+//     }
+// }
 
-}
+// void setup(){
+//     Serial.begin(9600);
+//     delay(2000);
+//     FFT_test();
+// }
 
+// void loop(){
 
-
-
-
-void setup(){
-    Serial.begin(9600);
-}
-
-void loop(){
-
-}
+// }
